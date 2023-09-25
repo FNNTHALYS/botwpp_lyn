a3c216877e1e5a43f8bc3aa67f19715395ccba84
FNNTHALYS <105325980+FNNTHALYS@users.noreply.github.com>
2023-09-24 23:31:15 -0300

    Update cardapioLoja.js

--- src/cardapioLoja.js
+++ src/cardapioLoja.js
@@ -24,5 +24,41 @@ export const cardapioLoja = {
     descricao: 'Macaxeira com frango',
     preco: 18.49,
   },
-
+  7: {
+    descricao: 'Parmegiana de Carne',
+    preco: 18.49,
+  },
+  8: {
+    descricao: 'Parmegiana de frango',
+    preco: 18.49,
+  },
+  9: {
+    descricao: 'Lasanha de carne',
+    preco: 18.49,
+  },
+  10: {
+    descricao: 'Lasanha de frango',
+    preco: 18.49,
+  },
+  11: {
+    descricao: 'Lasanha de camarão',
+    preco: 18.49,
+  },
+  12: {
+    descricao: 'Escondidinho de carne de sol',
+    preco: 18.49,
+  },
+  13: {
+    descricao: 'Escondidinho de charque',
+    preco: 18.49,
+  },
+  14: {
+    descricao: 'Escondidinho de camarão',
+    preco: 18.49,
+  },
+  15: {
+    descricao: 'Sopa de Costela',
+    preco: 18.49,
+  },
+  
 };
